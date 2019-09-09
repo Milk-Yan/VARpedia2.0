f47b84390b7c503483771c78acea64739f50dec5
Elisa Yansun <eyan868@en-369725.uoa.auckland.ac.nz>
2019-09-09 15:58:05 +1200

    Created scenes for creation

---
+++ src/main/java/application/CreateCreationSearchController.java
@@ -0,0 +1,30 @@
+package main.java.application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class CreateCreationSearchController {
+	@FXML
+	private Text _wikitTermEnquiryText;
+	
+	@FXML
+	private TextField _wikitTerm;
+	
+	@FXML
+	private Button _enterBtn;
+	
+	@FXML
+	private Button _mainMenuBtn;
+	
+	@FXML
+	private void enter() {
+		
+	}
+	
+	@FXML
+	private void mainMenu() {
+		
+	}
+}
