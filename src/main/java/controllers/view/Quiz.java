c11bc5a9b17b92cc36668ebee17ab349662b18ca
OverCry <wcho400@en-368398.uoa.auckland.ac.nz>
2019-10-10 17:35:33 +1300

    Added return from quiz scene

--- src/main/java/controllers/view/Quiz.java
+++ src/main/java/controllers/view/Quiz.java
@@ -1,8 +1,15 @@
 package main.java.controllers.view;
 
+import javafx.fxml.FXML;
 import main.java.controllers.Controller;
 
 public class Quiz extends Controller {
 
 
+    @FXML
+    private void mainMenuPress() {
+
+        mainMenu();
+
+    }
 }
