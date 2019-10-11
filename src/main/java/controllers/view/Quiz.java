60317ca74144f0192fb6718f828f69c9ab5f65f5
OverCry <wcho400@en-369731.uoa.auckland.ac.nz>
2019-10-11 14:49:03 +1300

    DARRRRRK

--- src/main/java/controllers/view/Quiz.java
+++ src/main/java/controllers/view/Quiz.java
@@ -3,7 +3,10 @@ package main.java.controllers.view;
 import javafx.fxml.FXML;
 import main.java.controllers.Controller;
 
+import java.util.Random;
+
 public class Quiz extends Controller {
+
 
 
     @FXML
